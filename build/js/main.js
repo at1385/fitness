e14bbb628d88a8110a0edcdcbdd82806a7a7a305
Alexander Tikhonov <at1385@yandex.ru>
2021-08-12 08:53:35 +0500

    Добавляет текущее состояние папки build

---
+++ build/js/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+(function () {
+  const gymVideoContainer = document.querySelector('.gym__video');
+  const gymVideoPlayer = gymVideoContainer.querySelector('video');
+  const gymVideoLink = gymVideoContainer.querySelector('.gym__video-link');
+
+  gymVideoLink.addEventListener('click', function(evt) {
+    evt.preventDefault();
+    gymVideoLink.classList.add('gym__video-link--hidden');
+    gymVideoPlayer.play();
+  });
+
+  gymVideoPlayer.addEventListener('ended', function() {
+    gymVideoLink.classList.remove('gym__video-link--hidden');
+  });
+})();
